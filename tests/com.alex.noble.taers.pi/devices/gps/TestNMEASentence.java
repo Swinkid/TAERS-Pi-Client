e45143af9481bd29f87d82cd8843e1899bdc9006
Alex Noble <swinkid@googlemail.com>
2017-03-15 22:39:28 +0000

    Fixing tests

--- tests/com.alex.noble.taers.pi/devices/gps/TestNMEASentence.java
+++ tests/com.alex.noble.taers.pi/devices/gps/TestNMEASentence.java
@@ -14,7 +14,7 @@ public class TestNMEASentence {
 
     private static final String TEST_NMEA_STRING = "$GPGGA,123519,4807.038,N,01131.000,E,1,08,0.9,545.4,M,46.9,M,,*47";
 
-    private static final String ACTUAL_SENTENCE_TYPE = "$GPGGA!!";
+    private static final String ACTUAL_SENTENCE_TYPE = "$GPGGA";
 
     private static NMEASentence nmeaSentence;
 
